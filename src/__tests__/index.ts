fbcc23288634810d8a2c409b9f364e646cd74edb
Ivan Demin <idemin@MacBook-Pro.local>
2022-02-19 18:29:28 +0300

    chore: fix test

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -2,6 +2,6 @@ import { multiply } from '../index';
 
 test('Тестирование функции multiply', () => {
   expect(multiply(0, 10)).toBe(0);
-  expect(multiply(1, 13)).toBe(14);
+  expect(multiply(1, 13)).toBe(13);
   expect(multiply(4, 7)).toBe(28);
 });
